9e66be0075c6439d479e4dc4b8921c8bacbe4bfa
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-12-05 17:09:52 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-ReactDOM.render(
-  <html>
-    <App />
-  </html>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));

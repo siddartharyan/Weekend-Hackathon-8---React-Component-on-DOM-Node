523f8059f54e469f8884fb33d0db5d70bc13feee
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-12-05 17:15:14 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 export default function App() {
   return (
-    <html>
-      <p>
-        Now I can render any React component on any DOM node I want using
-        ReactDOM.render
-      </p>
-    </html>
+    <p>
+      Now I can render any React component on any DOM node I want using
+      ReactDOM.render
+    </p>
   );
 }
